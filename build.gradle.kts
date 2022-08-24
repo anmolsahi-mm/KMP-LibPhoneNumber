444ff78c807219b68de3bc178f042b717bdc9e28
anmolsinghsahi <anmol.sahi@mutualmobile.com>
2022-08-24 18:20:44 +0530

    initial commit: setup basic project with all the required targets

---
+++ build.gradle.kts
@@ -0,0 +1,80 @@
+plugins {
+    kotlin("multiplatform") version "1.7.10"
+}
+
+group = "me.anmolsinghsahi"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+        withJava()
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    js(BOTH) {
+        browser {
+            commonWebpackConfig {
+                cssSupport.enabled = true
+            }
+        }
+    }
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    
+    iosArm32 {
+        binaries {
+            framework {
+                baseName = "library"
+            }
+        }
+    }
+    iosArm64 {
+        binaries {
+            framework {
+                baseName = "library"
+            }
+        }
+    }
+    iosX64 {
+        binaries {
+            framework {
+                baseName = "library"
+            }
+        }
+    }
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val jvmMain by getting
+        val jvmTest by getting
+        val jsMain by getting
+        val jsTest by getting
+        val nativeMain by getting
+        val nativeTest by getting
+        val iosArm32Main by getting
+        val iosArm32Test by getting
+        val iosArm64Main by getting
+        val iosArm64Test by getting
+        val iosX64Main by getting
+        val iosX64Test by getting
+    }
+}
